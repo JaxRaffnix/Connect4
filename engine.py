c34e11676b8e00e3993c3fa697579b82c9141527
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2023-03-28 08:09:59 +0200

    started work on undo button

--- engine.py
+++ engine.py
@@ -24,7 +24,6 @@ def board_index(board):
         indices = indices + str(i) + "\t"
     return indices
     
-    
 def setmark(board, player, choice):
     for i in reversed(range(len(board))):
         if board[i][choice] == player_mark(1) or board[i][choice] == player_mark(2):
@@ -36,54 +35,59 @@ def setmark(board, player, choice):
 def player_mark(player):
     if player == 1:
         return library.SYMBOL_1
-    if player == 2:
+    elif player == 2:
         return library.SYMBOL_2
 
 def player_switch(player):
     if player == 1:
         return 2
-    if player == 2:
+    elif player == 2:
         return 1
 
 def player_status(player):
     return "Current Player: " + player_mark(player)
 
 def turn_print(turn):
-    return "Current Turn: " + str(turn)
+    return "Turn: " + str(turn)
 
 def board_size(board):
     return len(board[0]) +1
 
 def check_win(board, player):
-    # horizontal
     for i in range(len(board)):
         for j in range(len(board[i])):
             try:
+                # horizontal
                 if player_mark(player) == board[i][j] == board[i][j+1] == board[i][j+2] == board[i][j+3]:
                     return True
-            except IndexError:
-                continue
-    # vertical
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            try:
-                if player_mark(player) == board[i][j] == board[i-1][j] == board[i-2][j] == board[i-3][j]:
+                # vertical            
+                elif player_mark(player) == board[i][j] == board[i-1][j] == board[i-2][j] == board[i-3][j]:
                     return True
-            except IndexError:
-                continue
-    # diagonal descending
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            try:
-                if player_mark(player) == board[i][j] == board[i-1][j+1] == board[i-2][j+2] == board[i-3][j+3]:
+                # diagonal left-right ascending
+                elif player_mark(player) == board[i][j] == board[i+1][j-1] == board[i+2][j-2] == board[i+3][j-3]:
                     return True
-            except IndexError:
-                continue
-    # diagonal ascending
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            try:
-                if player_mark(player) == board[i][j] == board[i+1][j-1] == board[i+2][j-2] == board[i+3][j-3]:
+                # diagonal right-left descending
+                elif player_mark(player) == board[i][j] == board[i-1][j+1] == board[i-2][j+2] == board[i-3][j+3]:
                     return True
+                # diagonal left-right descending
+                elif player_mark(player) == board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3]:
+                    return True
+                # diagonal right-left ascending
+                elif player_mark(player) == board[i][j] == board[i-1][j-1] == board[i-2][j-2] == board[i-3][j-3]:
+                    return True
+                else:
+                    return False
             except IndexError:
                 continue
+
+def undo(board, choice):
+    for i in reversed(range(len(board))):
+        try:
+            if board[i][choice] == player_mark(1) or board[i][choice] == player_mark(2):
+                continue
+            else:
+                board[i+1][choice] = "-"
+        except IndexError:
+            continue        
+        return board
+        
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Goal: Create the game connect 4 for twoplayermode and singleplayer mode against AI
-
+# import os
+from time import sleep
 import engine as eg
 import PySimpleGUI as sg
 
@@ -8,13 +9,15 @@ board = eg.board_initialise()
 # start values
 player_current = 1
 turn = 1
+# undo_locked = False
+history = []
 
 layout = [[sg.Text(eg.player_status(player_current), key="-PLAYER-"), sg.Push(), sg.Text(eg.turn_print(turn), key="-TURN-")],
           [sg.Text(eg.board_print(board), key="-BOARD-")],
           [sg.Text(eg.board_index(board))],
         #   [(sg.Button(str(i)), sg.Text("\t")) for i in range(1,8)]]
           [sg.Button(i) for i in range(1,eg.board_size(board))]]
-layout += [[sg.Button("Abort Round"), sg.Button("Exit Game")]]
+layout += [[sg.Button("Undo"), sg.Button("Abort Round"), sg.Button("Exit Game")]]
 
 window = sg.Window("Connect 4", layout, scaling = 2.5)
 
@@ -22,18 +25,34 @@ while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, "Abort Round", "Exit Game"):
         break
-    if event in ["1","2","3","4","5","6","7"]:
-    # if event in str(range(1,eg.board_size(board))):
+    elif event == "Undo":
+        # if undo_locked == True:
+            # print("You can only undo the last move.")
+            # continue
+        window["-BOARD-"].update(eg.board_print(eg.undo(board, history[turn-2])))
+        turn -= 1
+        window["-TURN-"].update(eg.turn_print(turn))
+        # undo_locked = True
+        continue
+    elif event in ["1","2","3","4","5","6","7"]:
         try:
             choice = int(event) - 1
+            history.append(choice)
             window['-BOARD-'].update(eg.board_print(eg.setmark(board, player_current, choice)))    
         except TypeError:
             print("Row {} has been filled. Choose a different row.".format(choice+1))
             continue  
         if eg.check_win(board, player_current)== True:
             print("Win for player {}!".format(eg.player_mark(player_current)))
+            turn += 1
+            window["-TURN-"].update(eg.turn_print(turn))
+            window.refresh()
+            sg.popup("Testx")
+            sleep(3)
+            break
         player_current = eg.player_switch(player_current)
         window["-PLAYER-"].update(eg.player_status(player_current))
         turn += 1
         window["-TURN-"].update(eg.turn_print(turn))
+        undo_locked = False
 window.close()
